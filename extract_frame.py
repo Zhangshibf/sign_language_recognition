bf21376c2946bf078fd8dbc03058022e7e9b987e
张史小冰风 <61115932+Zhangshibf@users.noreply.github.com>
2022-07-13 17:48:47 +0200

    Update extract_frame.py

--- extract_frame.py
+++ extract_frame.py
@@ -1,33 +1,26 @@
 import os
 import cv2
+import argparse
 
 def extractImages(pathIn, pathOut):
     for root, dirs, files in os.walk(pathIn, topdown=False):
         for name in files:
-            a=123
             video_path = os.path.join(root, name)
-#            count = 0
+            count = 0
             n = 1
             vidcap = cv2.VideoCapture(video_path)
             success,image = vidcap.read()
             while success:
-                directory = pathOut
-                os.chdir(directory)
-                name = name.replace(".mp4","")
-                filename = "{filename}_{num}.jpg".format(filename=name, num=n)
-                cv2.imwrite(filename, image)  # save frame as JPEG file
-                success, image = vidcap.read()
-
-#                vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line
-#                success,image = vidcap.read()
-#                if success==True:
-#                    directory = pathOut
-#                    os.chdir(directory)
-#                    name = name.replace(".mp4","")
-#                    filename = "{filename}_{num}.jpg".format(filename=name, num=n)
-#                    cv2.imwrite(filename, image)
-#                    count = count + 0.1
-#                    n+=1
+                vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line
+                success,image = vidcap.read()
+                if success==True:
+                    directory = pathOut
+                    os.chdir(directory)
+                    name = name.replace(".mp4","")
+                    filename = "{filename}_{num}.jpg".format(filename=name, num=n)
+                    cv2.imwrite(filename, image)
+                    count = count + 0.2
+                    n+=1
 
 if __name__=="__main__":
     a = argparse.ArgumentParser()
