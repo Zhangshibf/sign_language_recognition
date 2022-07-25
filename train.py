1b7ab1993f6e45c15c8d1246bcf22eede3300250
Zhangshibf <zhangshibf@gmail.com>
2022-07-25 12:17:32 +0200

    not done yet

---
+++ train.py
@@ -0,0 +1,7 @@
+import model
+import argparse
+if __name__=="__main__":
+    a = argparse.ArgumentParser()
+    a.add_argument("--pathDataset", help="path to the Dataset")
+    args = a.parse_args()
+    model.cross_val(args.pathDatset)
