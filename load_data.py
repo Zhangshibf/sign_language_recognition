e98547be07304ddb149a6e51eeaa13fbc9693d87
Zhangshibf <zhangshibf@gmail.com>
2022-08-03 15:37:43 +0200

    both hands

--- load_data.py
+++ load_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 import argparse
 
 
-def normalize_dataset(pathH,pathB,path_dataset):
+def create_dataset(pathH,pathB,path_dataset):
     hand = pd.read_csv(pathH,sep=",") #each frame correspond to a 64-dimensional vector
 
     #normalization
@@ -23,27 +23,56 @@ def normalize_dataset(pathH,pathB,path_dataset):
     body_24_pd["index"] = body.iloc[:, 0]
 
     #concatenate hand data with body data
-    """
-    Every frame has a body position estimation, however, about one third of frames does not have hand keypoint data because the recognition wasn't successful
-    Say that we have 5 frames for one video, namely a, b, c, d, e
-    body keypoint vectors, w1, w2, w3, w4, w5
-    only 2 hand keypoint vectors v2,v4
-    v1, v3 and v5 are going to be the averaged values of hand df
-    """
+    idx_both = ['029', '031', '032', '034', '035', '036', '043', '044', '045', '048', '049', '050', '051', '053', '054',
+                '055', '056', '057', '058', '060', '061', '063']
+    idx_left = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010', '011', '012', '013', '014', '015',
+                '016', '017', '018', '019', '020', '021', '022', '023', '024', '025', '026', '027', '028', '030', '033',
+                '037', '038', '039', '040', '041', '042', '046', '047', '052', '059', '062', '064']
+
     dataset = list()
     body_idx = list(body_24_pd["index"])
     hand_idx = list(hand_24_pd["index"])
+#    hand_zero = ["0"] * 24
     for b_idx in body_idx:
-        row = list()
-        row.append(b_idx)
-        if b_idx in hand_idx:
-            row.extend([str(i) for i in body_24_pd.loc[body_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
-            row.extend([str(i) for i in hand_24_pd.loc[body_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
-            dataset.append(row)
-        else:
-            row.extend([str(i) for i in body_24_pd.loc[body_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
-            row.extend(hand_mean_str)
-            dataset.append(row)
+
+        if b_idx[:3] in idx_left:
+            row = list()
+            row.append(b_idx)
+            if b_idx in hand_idx:
+                row.extend(
+                    [str(i) for i in body_24_pd.loc[body_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
+                row.extend(
+                    [str(i) for i in hand_24_pd.loc[hand_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
+                dataset.append(row)
+            else:
+                row.extend(
+                    [str(i) for i in body_24_pd.loc[body_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
+                row.extend(hand_mean_str)
+                dataset.append(row)
+
+        elif b_idx[:3] in idx_both:
+            row1 = list()
+            row1.append(b_idx)
+            row2 = list()
+            row2.append(b_idx)  # both hand frames need two rows
+
+            if b_idx in hand_idx:
+                row1.extend(
+                    [str(i) for i in body_24_pd.loc[body_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
+                row2.extend(
+                    [str(i) for i in body_24_pd.loc[body_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
+
+                h_vector = [str(i) for i in hand_24_pd.loc[hand_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]]
+                row1.extend(h_vector[:24])
+                row2.extend(h_vector[25:])
+                dataset.append(row1)
+                dataset.append(row2)
+            else:
+                row1.extend(
+                    [str(i) for i in body_24_pd.loc[body_24_pd['index'] == b_idx].values.flatten().tolist()[:-1]])
+                row1.extend(hand_mean_str)
+                dataset.append(row1)
+                dataset.append(row1)
 
     #and now the dataset is ready!
     f = open(path_dataset, "a", encoding="utf-8")
@@ -60,7 +89,7 @@ if __name__=="__main__":
     a.add_argument("--path_dataset", help="path to the files where you'd like to save the data")
     args = a.parse_args()
     print(args)
-    normalize_dataset(args.pathH,args.pathB,args.path_dataset)
+    create_dataset(args.pathH,args.pathB,args.path_dataset)
 
 
 
