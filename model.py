41f6398fa0e7c6dde46ea5aa55be89e3f029ad6d
Zhangshibf <zhangshibf@gmail.com>
2022-08-04 10:32:25 +0200

    softmax

--- model.py
+++ model.py
@@ -2,9 +2,8 @@ from collections import Counter
 import torch
 import torch.nn as nn
 from torch.nn import functional
-from torch.nn import Module
 import torch.optim as optim
-from torch.utils.data import DataLoader,Dataset
+#from torch.utils.data import DataLoader,Dataset
 
 
 class Dataset():
@@ -98,7 +97,7 @@ class sign_translator(nn.Module):
         super(sign_translator, self).__init__()
         self.layer1 = nn.LSTM(48,hidden_size,bidirectional = bidirectional,num_layers=num_layers,dropout=dropout)      #The LSTM layer
         self.layer2 = nn.Linear(hidden_size*2 if bidirectional == True else hidden_size,output_size)  #The linear layer
-        self.layer3 = nn.Softmax(output_size,64)   #The output layer with softmax
+        self.layer3 = nn.Softmax()   #The output layer with softmax
 
     def forward(self, vectors):
         # reshape the input for LSTM layer. The size of the expected input is [sequence length x 1 x 24]
@@ -106,7 +105,7 @@ class sign_translator(nn.Module):
         output_layer1, (hidden, cell) = self.layer1(video_input )
         output_layer2 = self.layer2(output_layer1)
         prediction = self.layer3(output_layer2)
-
+        print(prediction)
 
         return prediction
 
