f40b7a8a0b25e7a289231c65490d0dd40a0b06c3
Zhangshibf <zhangshibf@gmail.com>
2022-08-08 13:02:38 +0200

    a

--- load_dataset.py
+++ load_dataset.py
@@ -17,10 +17,12 @@ class Dataset():
             idxs.append(row_data[0])
             features.append(row_data[1:])
 
-        for row in features:
+        for row,idx in zip(features,idxs):
             if len(row)!=48:
-                print("There is something wrong")
                 print(len(row))
+                print(idx)
+
+        break
         # each row corresponds to a frame. Now we need to group all frame data of one video together. Each video should have only one label
         # 047_001_001_18.jpg
         videos = list(Counter([i[:11] for i in idxs]))
