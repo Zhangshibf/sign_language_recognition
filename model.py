a80d45b09b6c649218cd212905d2d358302dad22
Zhangshibf <zhangshibf@gmail.com>
2022-08-08 11:54:18 +0200

    a

--- model.py
+++ model.py
@@ -186,4 +186,4 @@ if __name__=="__main__":
     a = argparse.ArgumentParser()
     a.add_argument("--pathDataset", help="path to the pickled dataset object")
     args = a.parse_args()
-    model.cross_val(args.pathDataset)
+    cross_val(args.pathDataset)
