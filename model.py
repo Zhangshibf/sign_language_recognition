4d5d36fe311d4d154ea681ecdd4c2ad986aa1ff7
Zhangshibf <zhangshibf@gmail.com>
2022-08-08 11:22:17 +0200

    deleted the dataset part

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from collections import Counter
+
 import torch
 import torch.nn as nn
 from torch.nn import functional
@@ -6,87 +6,7 @@ import torch.optim as optim
 #from torch.utils.data import DataLoader,Dataset
 
 
-class Dataset():
 
-    def __init__(self, path_dataset):
-        instances = list()
-        labels = list()
-        f = open(path_dataset)
-        data = f.read()
-        rows = data.split("\n")
-        idxs = list()
-        features = list()
-        for row in rows:
-            row_data = row.split(",")
-            idxs.append(row_data[0])
-            features.append(row_data[1:])
-
-        # each row corresponds to a frame. Now we need to group all frame data of one video together. Each video should have only one label
-        # 047_001_001_18.jpg
-        videos = list(Counter([i[:11] for i in idxs]))
-        videos.sort()
-        videos = videos[1:]
-
-        for video_name in videos:
-            labels.append(video_name)
-            # find all frames of the same video, group them together. That's an instance
-            frame_names = [i for i in idxs if video_name in i]
-            frame_names.sort()  # to make sure an instance is composed of [frame1,frame2,frame3...] in stead of random order
-            frame_idxs = [i for i, x in enumerate(idxs) if x in frame_names]
-            video_feature = list()
-            for frame_idx in frame_idxs:
-                video_feature.append(features[frame_idx])
-            instances.append(video_feature)
-
-        targets = self.create_targets(labels)
-        signer = self.create_signer_list(labels)
-
-        self.signers = signer
-        self.labels = targets
-        self.instances = instances
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        label = self.labels[idx]
-        instance = self.instances[idx]
-        sample = {"Data": instance, "Class": label}
-
-        return sample
-
-    def create_test_set(self):
-        # signer 10 is used as test set
-        idx_signer10 = [i for i, x in enumerate(self.signers) if x == 10]
-        self.test_x = [self.instances[idx] for idx in idx_signer10]
-        self.test_y = [self.labels[idx] for idx in idx_signer10]
-
-    def train_dev_split(self, dev):
-        # dev is the ID of the signer to be used as dev set.
-        idx_signer_dev = [i for i, x in enumerate(self.signers) if x == dev]
-        self.dev_x = [self.instances[idx] for idx in idx_signer_dev]
-        self.dev_y = [self.labels[idx] for idx in idx_signer_dev]
-
-        idx_signer_train = [i for i, x in enumerate(self.signers) if x not in [dev, 10]]
-        self.train_x = [self.instances[idx] for idx in idx_signer_train]
-        self.train_y = [self.labels[idx] for idx in idx_signer_train]
-        #这里得加个shuffle
-
-    def create_targets(self, idx):
-        y = list()
-        for i in idx:
-            video = i[:3]
-            video_int = int(video.lstrip("0"))
-            y.append(video_int)
-        return y
-
-    def create_signer_list(self, idx):
-        signers = list()
-        for i in idx:
-            signer = i[4:7]
-            signer_int = int(signer.lstrip("0"))
-            signers.append(signer_int)
-        return signers
 
 
 
@@ -120,6 +40,7 @@ def train_model(model,x,y,optimizer,loss_function):
     for train_x,train_y in zip(x,y):
         optimizer.zero_grad()
         model_prediction = model(train_x)
+        #这里肯定要改
         model_prediction = torch.reshape(model_prediction, [model_prediction.shape[0], model_prediction.shape[2]])
         loss_per_batch = loss_function(model_prediction, train_y)
         epoch_accuracy += calculate_accuracy_per_batch(model_prediction, train_y)
@@ -165,8 +86,7 @@ def evaluate_model(model, x,y, loss_function):
         print(f"The averaged accuracy is {accuracy}")
 
 
-def cross_val(pathDataset,lr= 0.0001):
-    dataset = Dataset(pathDataset)
+def cross_val(dataset,lr= 0.0001):
     dataset.create_test_set()
     loss_function = nn.functional.cross_entropy
     for i in range(1,10):
