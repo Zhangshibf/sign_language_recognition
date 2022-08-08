f6d6c21ac25e0687ea514ed25a8b9d924c798bab
Zhangshibf <zhangshibf@gmail.com>
2022-08-08 19:27:23 +0200

    a

--- model.py
+++ model.py
@@ -106,7 +106,7 @@ class sign_translator(nn.Module):
         print("output_layer2")
         print(output_layer2)
         print(output_layer2.size())
-        last_output = output_layer2[0][-1]
+        last_output = output_layer2[0][0]
         print(last_output)
         #we should take the last output of layer2.
         prediction = self.layer3(output_layer2)
