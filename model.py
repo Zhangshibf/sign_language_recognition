05f3b0e5e6895117dc943953c48854323ac2d34b
Zhangshibf <zhangshibf@gmail.com>
2022-08-11 22:27:10 +0200

    gpu

--- model.py
+++ model.py
@@ -200,6 +200,7 @@ def train_test(pathDataset,lr= 0.001,epoch=20):
     with open(pathDataset, 'rb') as inp:
         dataset = pickle.load(inp)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
     loss_function = nn.functional.cross_entropy
     model = sign_translator(hidden_size=64, output_size=64)
     model.to(device)
