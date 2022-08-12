1fbad29b32e897cb0f75626652d2e8ddaf318643
张史小冰风 <61115932+Zhangshibf@users.noreply.github.com>
2022-08-12 15:57:10 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import model as m
-import argparse
-
-if __name__=="__main__":
-    a = argparse.ArgumentParser()
-    a.add_argument("--pathInH", help="path to the hand keypoint dataset file")
-    a.add_argument("--pathInB", help="path to the body keypoint dataset file")
-    args = a.parse_args()
-    m.train_model(args.pathInH, args.pathInB)
