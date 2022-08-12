4fe5a71339a0d667037e81bd1b13c26f01e266ab
张史小冰风 <61115932+Zhangshibf@users.noreply.github.com>
2022-08-12 15:57:02 +0200

    Delete train.py

--- train.py
+++
@@ -1,7 +0,0 @@
-import model
-import argparse
-if __name__=="__main__":
-    a = argparse.ArgumentParser()
-    a.add_argument("--pathDataset", help="path to the pickled dataset object")
-    args = a.parse_args()
-    model.cross_val(args.pathDataset)
